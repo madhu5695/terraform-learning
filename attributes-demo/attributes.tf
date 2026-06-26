ff5459be3a79e8dd694c28ccea7197b4f2eb9a7e
Madhu <madhuravalli.vivekanandan@gmail.com>
2026-06-26 08:15:55 +0530

    Creating ec2 and static Ip using terraform to check on attributes

---
+++ attributes-demo/attributes.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+resource "aws_instance" "myec2" {
+   ami = "ami-08f44e8eca9095668"
+   instance_type = "t3.micro"
+   tags = {
+    Name = "my-first-ec2"
+   }
+}
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
